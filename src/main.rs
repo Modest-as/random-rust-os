f36ebb91c19ce569102d523bf9c44eca7c91c795
Modest-as <modestas.petravicius@gmail.com>
2019-03-02 22:16:44 +0200

    unit tests

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 // src/main.rs
-#![no_std] // don't link the Rust standard library
-#![no_main] // disable all Rust-level entry points
+
+#![cfg_attr(test, allow(unused_imports))] // disable compile warnings in test mode
+#![cfg_attr(not(test), no_std)] // don't link the Rust standard library
+#![cfg_attr(not(test), no_main)] // disable all Rust-level entry points
+
+#[cfg(test)]
+extern crate array_init;
+#[cfg(test)]
+extern crate core;
 
 extern crate lazy_static;
 extern crate spin;
@@ -10,13 +17,16 @@ mod vga_buffer;
 
 use core::panic::PanicInfo;
 
-/// This function is called on panic.
+/// This function is called on panic
+/// only compile when the test flag is not set
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
 }
 
+#[cfg(not(test))]
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
     // this function is the entry point, since the linker looks for a function
